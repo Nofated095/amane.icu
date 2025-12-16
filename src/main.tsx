47d5d70478768662ab19fdae22f4a78d8cd13c73
Nofated095 <nofated095@outlook.com>
2025-12-16 17:17:52 +0800

    add service worker.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,18 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import AutoTheme from './theme';
 import App from './App';
+// @ts-ignore: virtual module provided by Vite PWA
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    console.log('refresh cache.')
+  },
+  onOfflineReady() {
+    console.log('offline ready.')
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerStrategy: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,gif}'],
+      },
+    }),
+  ],
 });
