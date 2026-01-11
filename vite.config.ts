dbb4e4bffc0c4dd154a77cea918097362c2b522c
Nofated095 <nofated095@outlook.com>
2026-01-11 22:40:05 +0800

    外部链接加入 service worker 缓存

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,50 @@ export default defineConfig({
       registerStrategy: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,webp,svg,gif,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/cloudflare\.endpoint\.amaneus\.net\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'avatar-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/vercel\.endpoint\.amaneus\.net\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'vercel-redirect-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/pic\.rmb\.bdstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'favicon-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
       },
     }),
   ],
